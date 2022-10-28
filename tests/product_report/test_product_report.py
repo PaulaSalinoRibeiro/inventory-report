3ec65221ec4fdf59e473cf8c9eda1293e52bc017
Paula <paulasalinor@gmail.com>
2022-10-28 15:03:28 -0300

    feat: add test in tests/product_report/test_product_repost.py

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Produto",
+        "Empresa",
+        "28-10-2022",
+        "28-10-2023",
+        "xml123",
+        "Armazenamento",
+    )
+
+    assert product.__repr__() == (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} com validade "
+        f"até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
+    assert type(product.__repr__()) == str
