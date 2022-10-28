b2862ac562f7214f5f4eeb4557406d6dade13266
Paula <paulasalinor@gmail.com>
2022-10-28 16:25:19 -0300

    feat: add SimpleReport class in inventory_report/reports/simple_reports

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from collections import Counter
+
+
+class SimpleReport:
+    @classmethod
+    def get_date_most_old(cls, list):
+        return min(
+            datetime.fromisoformat(item["data_de_fabricacao"]).date()
+            for item in list
+        )
+
+    @classmethod
+    def get_date_most_close(cls, list):
+        return min(
+            datetime.fromisoformat(item["data_de_validade"]).date()
+            for item in list
+            if datetime.fromisoformat(item["data_de_validade"])
+            > datetime.now()
+        )
+
+    @classmethod
+    def get_enterprise_most_products(cls, list):
+        return Counter(
+            [item["nome_da_empresa"] for item in list]
+        ).most_common()[0][0]
+
+    @classmethod
+    def generate(cls, list):
+        most_old = SimpleReport.get_date_most_old(list)
+        print(most_old)
+        most_close = SimpleReport.get_date_most_close(list)
+        print(most_close)
+        most_products = SimpleReport.get_enterprise_most_products(list)
+
+        return (
+            f"Data de fabridação mais antiga: {most_old} \n"
+            f"Data de validade mais próxima: {most_close} \n"
+            f"Empresa com mais produtos: {most_products}"
+        )
---
+++ inventory_report/reports/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+def get_date_most_close(self, list):
+    #     date_now = datetime.today()
+    #     return min(
+    #         datetime.fromisoformat(item["data_de_validade"])
+    #         for item in list
+    #         if item["data_de_fabricacao"] >= date_now
+    #     )
