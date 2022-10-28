b0f8235b1925523061db55ed3b9fe7cc7e23d3e2
Paula <paulasalinor@gmail.com>
2022-10-28 16:29:56 -0300

    fix: flask error syntaxe

--- inventory_report/reports/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def get_date_most_close(self, list):
-    #     date_now = datetime.today()
-    #     return min(
-    #         datetime.fromisoformat(item["data_de_validade"])
-    #         for item in list
-    #         if item["data_de_fabricacao"] >= date_now
-    #     )
