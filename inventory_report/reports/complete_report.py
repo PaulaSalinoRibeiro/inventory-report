e02f57306b9170f62c8277a9cedc9ef96836df88
Paula <paulasalinor@gmail.com>
2022-10-28 17:29:16 -0300

    feat: add CompleteReport class

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -17,11 +17,20 @@ class CompleteReport(SimpleReport):
 
     @classmethod
     def generate(cls, list):
+        most_old = super().get_date_most_old(list)
+
+        most_close = super().get_date_most_close(list)
+
+        most_products = super().get_enterprise_most_products(list)
+
         products_by_entreprise = CompleteReport.store_products_by_entreprise(
             list
         )
+
         return (
-            super().generate(list)
+            f"Data de fabricação mais antiga: {most_old}\n"
+            f"Data de validade mais próxima: {most_close}\n"
+            f"Empresa com mais produtos: {most_products}"
             + "\nProdutos estocados por empresa:\n"
             + products_by_entreprise
         )
