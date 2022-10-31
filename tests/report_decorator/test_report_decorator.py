3cc14e7d9ba51a5fb71751b63bae6c6905923768
Paula <paulasalinor@gmail.com>
2022-10-31 15:24:07 -0300

    feat: add test_decorar_relatorio

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,29 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    product_list = [
+        {
+            "id": 1,
+            "nome_do_produto": "Cafe",
+            "nome_da_empresa": "Cafes Nature",
+            "data_de_fabricacao": "2020-07-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48",
+            "instrucoes_de_armazenamento": "instrucao",
+        }
+    ]
+
+    report_color_simple = ColoredReport(SimpleReport)
+
+    report_simple = report_color_simple.generate(product_list)
+    assert (
+        report_simple
+        == "\033[32mData de fabricação mais antiga:\033[0m "
+        + "\033[36m2020-07-04\033[0m"
+        + "\n\033[32mData de validade mais próxima:\033[0m "
+        + "\033[36m2023-02-09\033[0m"
+        + "\n\033[32mEmpresa com mais produtos:\033[0m "
+        + "\033[31mCafes Nature\033[0m"
+    )
