7c3d515d958b0432fb32ffb30ab23369c3dfcd71
Paula <paulasalinor@gmail.com>
2022-10-31 19:05:04 -0300

    feat: add main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,25 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+
+    _, path, report_type = sys.argv
+
+    report = None
+
+    if path.endswith(".csv"):
+        report = InventoryRefactor(CsvImporter)
+
+    if path.endswith(".json"):
+        report = InventoryRefactor(JsonImporter)
+
+    if path.endswith(".xml"):
+        report = InventoryRefactor(XmlImporter)
+
+    print(report.import_data(path, report_type), end="")
